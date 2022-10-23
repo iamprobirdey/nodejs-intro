e2eb19d7384b9bf396951eb2d2537d0e52fe169a
Probir Dey <probir@Probirs-MacBook-Air.local>
2022-10-23 23:56:22 +0530

    package.json | nodemon | gitignore | installed

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Probir Dey');
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    res.write('Response written');
+    res.end();
+});
+
+
+console.log(`Server is running on http://localhost:3000`);
+
+server.listen(3000);

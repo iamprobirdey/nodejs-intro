608c05c5fb9155d4c7af79fae952b3ddd26aa3ec
Probir Dey <probir@Probirs-MacBook-Air.local>
2022-10-27 00:36:44 +0530

    express integrated

--- app.js
+++ app.js
@@ -1,8 +1,54 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.write('Response written');
-    res.end();
+    if(req.url === '/'){
+        res.writeHead(200, { "Content-Type" : "text/html" });
+
+        fs.readFile('pages/home.html', 'utf8' , (err, data) => {
+            if(err) throw err;
+
+            res.write(data);
+
+            res.end();
+        });
+    }
+    else if(req.url === '/about'){
+        res.writeHead(200,{ "Content-Type" : 'text/html' });
+        fs.readFile('pages/about.html', 'utf8', (err, data) => {
+            if(err) throw err;
+
+            res.write(data);
+
+            res.end();
+        });
+    }else if(req.url === '/create-file'){
+        res.writeHead(200, { 'Content-Type' : 'text/html' });
+
+        const content = '<h4> This is temp data</h4> \n';
+
+        for(let i = 0; i <= 100; i++){
+            fs.appendFile('temp/temp.html', content, err => {
+                if(err) throw err;
+            });
+        }
+        res.write('I am writting data');
+
+        res.end();
+
+    }
+    else{
+        res.writeHead(404, {
+            'Content-Type' : 'text/html'
+        });
+        fs.readFile(('pages/404.html'), (err, data) => {
+            if(err) throw err;
+
+            res.write(data);
+
+            res.end(); 
+        })
+    }
 });
 
 
---
+++ notes.md
@@ -0,0 +1,10 @@
+# Topics covered 
+    # 1. Http
+    # 2. File System
+    # 3. Event Loop Task Sequence.
+
+
+
+Sites to visit:
+
+nodejs.dev
